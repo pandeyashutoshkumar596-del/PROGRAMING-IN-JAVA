bc6f97dbe840dd4f5633ce1e75de8d0b0f8eb4df
Ashutosh Pandey <pandeyashutoshkumar596@gmail.com>
2026-09-05 14:43:12 +0530

    Add files via upload

---
+++ UNIT 2/2PROG13.JAVA
@@ -0,0 +1,16 @@
+
+ // write a java program of final keyword?
+
+
+ 
+public class unit2prog13{
+
+    public static void main(String[] args) {
+        final int rollnumber = 101;
+        final double Max_Marks = 10.10;
+
+        System.out.println(rollnumber);
+
+      //  rollnumber = 201;
+    }
+}
