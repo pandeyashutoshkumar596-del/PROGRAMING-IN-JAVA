9c58e73ba0f60ab200be91e6a1ae2696d9c9c3ee
Ashutosh Pandey <pandeyashutoshkumar596@gmail.com>
2026-09-05 13:10:40 +0530

    Add files via upload

---
+++ UNIT 1/PROG7_1.JAVA
@@ -0,0 +1,20 @@
+// 7_1 WRITE A JAVA PROGRAM OF ONLY USING IF CONDITION?
+
+import java.util.Scanner;
+
+public class prog7_1{
+		
+		public static void main(String args[]){
+		Scanner scn = new Scanner (System.in);
+		
+		System.out.println("enter you age:");
+		int age = scn.nextInt();
+		
+		if(age>18){
+		System.out.println("you are eligible to vote");
+		}
+		
+		}
+
+
+}
