c24f4d595a5df3f455b11c5a58b5aaac9b76dd94
Ashutosh Pandey <pandeyashutoshkumar596@gmail.com>
2026-09-05 13:15:36 +0530

    Add files via upload

---
+++ UNIT 1/PROG7_5.JAVA
@@ -0,0 +1,43 @@
+//prog7_5 WRITE A JAVA PROGRAM USING SWITCH CASE
+
+  public class prog7_5{
+	
+		public static void main(String args[]){
+		
+		int day = 2;
+		switch(day){
+		case 1:
+		System.out.print("monday");
+		break;
+		
+		case 2:
+		System.out.print("tuesday");
+		break;
+		
+		case 3:
+		System.out.print("wednesday");
+		break;
+		
+		case 4:
+		System.out.print("thursday");
+		break;
+		
+		case 5:
+		System.out.print("friday");
+		break;
+		
+		case 6:
+		System.out.print("saturday");
+		break;
+		
+		case 7:
+		System.out.print("sunday");
+		break;
+		
+		default:
+		System.out.print("not a valid case");
+		
+		}
+        }
+  
+  }
